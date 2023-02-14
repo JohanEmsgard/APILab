c2aaa1b64e9c02e2b89ff066b9aae088854b6bee
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2023-02-14 13:20:32 +0100

    sfsf

--- APILab/Program.cs
+++ APILab/Program.cs
@@ -1,5 +1,12 @@
 WebApplication app = WebApplication.Create();
 
+app.Urls.Add("http://localhost:3000");
+app.Urls.Add("http://*:3000");
+
+Villanins evil = new Villains
+
+
+
 app.MapGet("/",Answer);
 
 app.MapGet("/Villan/", () => 
