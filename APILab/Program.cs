b04d901d7baef1c4efea0c0e5a52c4733720dd81
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2023-02-15 12:28:26 +0100

    bvadva

--- APILab/Program.cs
+++ APILab/Program.cs
@@ -3,16 +3,17 @@ WebApplication app = WebApplication.Create();
 app.Urls.Add("http://localhost:3000");
 app.Urls.Add("http://*:3000");
 
-Villains evil = new Villains();
+List<Villains> evil = new();
 
-evil.Name = "Eggman";
-evil.IQ = 300;
+evil.Add(new(){Name="Eggman", IQ=300});
+evil.Add(new(){Name="Bowser", IQ=1});
+
 
 app.MapGet("/",Answer);
 
 app.MapGet("/Villan/", () => 
 {
-    return "Eggman";
+    return evil;
 });
 
 app.Run();
